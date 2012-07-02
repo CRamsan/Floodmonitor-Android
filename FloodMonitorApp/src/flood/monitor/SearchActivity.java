9bd69f23da0afe47d714953431407d92f4040843
Cesar.Ramirez <ramirez@cesarandres.com>
2012-07-02 12:28:18 -0500

    Adding search

---
+++ FloodMonitorApp/src/flood/monitor/SearchActivity.java
@@ -0,0 +1,21 @@
+package flood.monitor;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SearchActivity extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.search);
+
+		// Get the intent, verify the action and get the query
+		Intent intent = getIntent();
+		if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+			String query = intent.getStringExtra(SearchManager.QUERY);
+			doMySearch(query);
+		}
+	}
+}
---
+++ FloodMonitorApp/src/flood/monitor/overlay/LimitedMapView.java
@@ -0,0 +1,81 @@
+package flood.monitor.overlay;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.widget.Toast;
+
+import com.google.android.maps.MapView;
+
+import flood.monitor.MapViewActivity;
+
+public class LimitedMapView extends MapView {
+	// ========================================================================
+	// MEMBERS
+	// ========================================================================
+
+	private MapViewActivity activity;
+	private int mapLevel;
+
+	// ========================================================================
+	// CONSTRUCTORS
+	// ========================================================================
+
+	public LimitedMapView(Context context, String apiKey) {
+		super(context, apiKey);
+	}
+
+	public LimitedMapView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public LimitedMapView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+	}
+
+	// ========================================================================
+	// GETTERS / SETTERS
+	// ========================================================================
+
+	public void setMapLevel(int mapLevel) {
+		this.mapLevel = mapLevel;
+	}
+
+	public int getMapLever() {
+		return this.mapLevel;
+	}
+
+	// ========================================================================
+	// EVENT HANDLERS
+	// ========================================================================
+	@Override
+	public boolean onTouchEvent(MotionEvent ev) {
+		final int action = ev.getAction();
+		if (action == MotionEvent.ACTION_UP) {
+			switch (mapLevel) {
+			case MapViewActivity.MAP_LEVEL_EVENT:
+				if (this.getZoomLevel() > 10) {
+					this.getController().setZoom(10);
+					Toast.makeText(activity, "Choose a region to zoom further", Toast.LENGTH_LONG).show();
+				}
+				return true;
+			case MapViewActivity.MAP_LEVEL_REGION:
+				return true;
+			case MapViewActivity.MAP_LEVEL_MARKER:
+				return true;
+			default:
+				return super.onTouchEvent(ev);
+			}
+		} else {
+			return super.onTouchEvent(ev);
+		}
+	}
+
+	// ===========================================================
+	// Methods
+	// ===========================================================
+	public void updateActivity(MapViewActivity newActivity) {
+		this.activity = newActivity;
+		this.mapLevel = activity.getMapLevel();
+	}
+}
